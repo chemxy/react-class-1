38cb09c6280e316eeb356a8c604f69e70eec1a3b
Roger(Xingyun) Chen <44486666+chemxy@users.noreply.github.com>
2023-11-26 16:02:21 -0800

    update code practices

--- in-class practices/code-practice-pass-data-to-child-components.js
+++ in-class practices/code-practice-pass-data-to-child-components.js
@@ -9,18 +9,46 @@ function PassSimpleTextWithProps() {
 }
 
 function PassSimpleTextWithPropsChildComponent() {
+    // TODO: make changes to the code to create props and receive message from parent component and display it below
+
     return (
         <div className="code-practice-section">
             <h3>Pass Simple Text With Props</h3>
             <p> message to display: </p>
             <p>
-                {/*TODO: make changes to the code to create props to receive message from parent component and display it here*/}
+                TODO_MESSAGE
             </p>
         </div>
     )
 };
 
 /*============================== exercise 2 =================================*/
+function PassObjectsWithProps() {
+    const userData = {
+        name: 'Alice',
+        age: 25,
+        city: 'New York',
+    };
+
+    //TODO: make changes to the code to pass userData to <PassObjectsWithPropsChildComponent>
+    return <PassObjectsWithPropsChildComponent/>;
+};
+
+function PassObjectsWithPropsChildComponent() {
+    // TODO: make changes to the code to create props and receive data from parent component and display them below
+
+    return (
+        <div className="code-practice-section">
+            <h3>Pass Objects With Props</h3>
+            <p>name: TODO_NAME</p>
+            <p>age: TODO_AGE</p>
+            <p>city: TODO_CITY</p>
+        </div>
+    );
+}
+
+
+/*============================== exercise 3 =================================*/
 function PassContentWithProps() {
     return <PassContentWithPropsChildComponent>
         <p>this is an exercise</p>
@@ -29,12 +57,13 @@ function PassContentWithProps() {
 }
 
 function PassContentWithPropsChildComponent() {
+    // TODO: make changes to the code to create props and receive the content inside <PassContentWithPropsChildComponent>
 
     return (
         <div className="code-practice-section">
             <h3>Pass Content With Props</h3>
             <p>
-                {/*TODO: make changes to the code to create props to receive the content inside <PassContentWithPropsChildComponent>*/}
+                TODO_CONTENT
             </p>
             <button>Click me!</button>
         </div>
@@ -47,6 +76,7 @@ function App() {
         <div>
             <PassSimpleTextWithProps/>
             <PassContentWithProps/>
+            <PassObjectsWithProps/>
         </div>
     );
 }
